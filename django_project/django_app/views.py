8507a71a5c455fb9121532ec40b842449f69267b
João Marcelo Dantas <jmgrd98@gmail.com>
2023-05-30 16:27:53 -0300

    usuario model

--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Usuario
 
-# Create your views here.
 def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
-    return render(request, 'usuarios/usuarios.html')
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    return render(request, 'usuarios/usuarios.html', usuarios)
